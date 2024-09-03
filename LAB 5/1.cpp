376c6b419e34bf87fcd051ab6ffa696cc86225df
riyooo13 <chaudhary.dipika17@gmail.com>
2024-09-03 14:20:02 +0545

    File update

--- LAB 5/1.cpp
+++ LAB 5/1.cpp
@@ -1,244 +1,319 @@
-/*Define a class Library with data members ISBN, bookTitle, author, and numberOfCopies. Implement methods to add new books, issue books, return books, update book details, and delete books. The library's catalog should be maintained in a file. Specifically, implement the following operations:
-1. Add New Book: Insert a new book's details into the catalog file. Ensure that each book has a unique ISBN and that duplicate entries are not allowed.
-2. Issue Book: Mark a book as issued by decreasing the number of available copies in the catalog file. Ensure that a book cannot be issued if the number of available copies is
-zero.
-3. Return Book: Mark a book as returned by increasing the number of available copies in the catalog file.
-4. Update Book Details: Edit the details of a book, such as ISBN, book Title, author, or numberOfCopies, in the catalog file. Ensure that changing ISBN does not result in duplicates.
-5. Delete Book: Allow the user to delete a book's record from the catalog file only if the following conditions are met:
-- The number of available copies is zero.
-- The book has not been issued to any user, meaning all copies have been returned and there are no outstanding issues for the book.
-Ensure that the number of copies is appropriately updated for each operation and that file operations maintain data integrity. Provide methods to read from and write to the file for maintaining the catalog data.
+/*
+Define a class Library with data memberes ISBN, bookTitle, author, and
+numberOfCopies. Implement methods to add new books, issue books, return books,
+update book details, and delete books. The library's catalob should be maintained in a
+file. Specifically, implement the following operations:
+
+1.  Add New Book: Insert a new book's details into the catalog file. Ensure that each book
+    has unique ISBN and that duplicate entries are not allowed.
+2.  Issue Book: Mark a book as issued by decreasing the number of available copies in the
+    catalog file. Ensure that a book cannot be issued if the number of available copies is
+    zero.
+3.  Return Book: Mark a book as returned by increasing the number of available copies in
+    the catalog file.
+4.  Update Book Details: Edi the details of a bookk, such as ISBN, bookTitle, author,
+    or numberOfCopies, in the catalog file. Ensure that cahnging ISBN does not result in
+    duplicates.
+5.  Delete Book: Allow the user to delete a book's record from the catalog file only if the
+    following conditions are met:
+        -   The number of available copies is zero.
+        -   The book has not been issued to any user, meaning allcopies have been returned and
+            there are no outstanding issues for the book.
+
+Ensure that the number of copies is appropriately updated for each operation and that file
+operations maintain data integrity. Provide methods to read from and write to the file for
+maintaining the catalog data.
 */
 
-#include <iostream>
-#include <fstream>
-#include <string>
+#include<iostream>
+#include<fstream>
+#include<conio.h>
+#include<cstring>
 
 using namespace std;
 
-class Book {
-public:
+class Library{
     string ISBN;
-    string bookTitle;
-    string author;
     int numberOfCopies;
+    char bookTitle[50], author[30];
 
-    void inputBook() {
-        cout<< "Enter ISBN: ";
-        cin>> ISBN;
-        cout<< "Enter Book Title: ";
-        cin>> bookTitle;
-        cout<< "Enter Author: ";
-        cin>> author;
-        cout<< "Enter Number of Copies: ";
-        cin>> numberOfCopies;
-    }
+    public:
+        void getData(){
+            cout << "Enter ISBN: ";
+            cin >> ISBN;
 
-    void displayBook()  {
-        cout<< "ISBN: " << ISBN << endl;
-        cout<< "Title: " << bookTitle << endl;
-        cout<< "Author: " << author << endl;
-        cout<<"Copies: " << numberOfCopies << endl;
-    }
-};
+            cout << "Enter title: ";
+            cin.ignore();
+            cin.getline(bookTitle, 50);
 
-class Library {
-private:
-    string catName;
+            cout << "Enter author name: ";
+            cin.getline(author, 30);
 
-    bool bookExists( string& isbn) {
-        ifstream file(catName);
-        string line;
-        while (getline(file, line)) {
-            if (line.find(isbn) == 0) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    void writeBook(ofstream& file,  Book& book) {
-        file << book.ISBN << endl << book.bookTitle << endl << book.author << endl << book.numberOfCopies << endl;
-    }
-
-public:
-    Library(const string& fileName) : catName(fileName) {}
-
-    void addNewBook() {
-        Book newBook;
-        newBook.inputBook();
-
-        if (bookExists(newBook.ISBN)) {
-            cerr << "Book with ISBN " << newBook.ISBN << " already exists.\n";
-            return;
+            cout << "Enter the number of copies: ";
+            cin >> numberOfCopies;
+            cin.ignore();
         }
 
-        ofstream file(catName, ios::app);
-        if (!file) {
-            cerr << "Error! Could not open file.\n";
-            return;
+        void showData(){
+            cout << "Book Information" << endl;
+            cout << "ISBN: " << ISBN << endl;
+            cout << "Title: " << bookTitle << endl;
+            cout << "Author: " << author << endl;
+            cout << "Number of copies: " << numberOfCopies << endl;
         }
-        writeBook(file, newBook);
-    }
+        
+        void addBook() {
+            system("cls");
+            cout << "Add New Book" << endl;
+            getData();
 
-    void issueBook() {
-        cout << "Enter ISBN to issue: ";
-        string isbn;
-        cin>> isbn;
+            Library temp;
+            bool match = false;
+            ifstream inf("books.dat", ios::binary);
 
-        ifstream file(catName);
-        ofstream tempFile("temp.txt");
-
-        string line;
-        bool found = false;
-
-        while (getline(file, line)) {
-            Book book;
-            size_t pos = line.find(';');
-            book.ISBN = line.substr(0, pos);
-
-            if (book.ISBN == isbn) {
-                found = true;
-                book.numberOfCopies = stoi(line.substr(line.find_last_of(';') + 1)) - 1;
-                if (book.numberOfCopies < 0) {
-                    cerr << "No copies available.\n";
-                    book.numberOfCopies = 0;
+            if (!inf) {
+                ofstream outf("books.dat", ios::binary);
+                if (!outf) {
+                    cerr << "Error creating file!" << endl;
+                    return;
                 }
-            } else {
-                tempFile << line << endl;
+                outf.write(reinterpret_cast<const char *>(this), sizeof(*this));
+                outf.close();
+                cout << "Book added successfully." << endl;
+                return;
             }
+
+            while (!inf.eof()) {
+                inf.read(reinterpret_cast<char *>(&temp), sizeof(temp));
+                if (temp.ISBN == ISBN) {
+                    match = true;
+                    cout << "ISBN is repeated." << endl;
+                    break;
+                }
+            }
+            inf.close();
+
+            if (!match) {
+                ofstream outf("books.dat", ios::app | ios::binary);
+                if (!outf) {
+                    cerr << "Error to open file!" << endl;
+                    return;
+                }
+                outf.write(reinterpret_cast<const char *>(this), sizeof(*this));
+                outf.close();
+                cout << "Book added successfully!" << endl;
+            }
+        
+        }
+
+        void issueBook(){
+            system("cls");
+            string isbn;
+            cout << "Enter the ISBN to issue: ";
+            cin >> isbn;
+
+            fstream file("books.dat", ios::binary | ios::in | ios::out);
+            if (!file) {
+                cerr << "Error opening file!" << endl;
+                return;
+            }
+
+            bool found = false;
+            while (file.read(reinterpret_cast<char*>(this), sizeof(*this))) {
+                if (ISBN == isbn) {
+                    found = true;
+                    if (numberOfCopies > 0) {
+                        numberOfCopies--;
+                        file.seekp(-static_cast<int>(sizeof(*this)), ios::cur);
+                        file.write(reinterpret_cast<char*>(this), sizeof(*this));
+                        cout << bookTitle << " issued." << endl;
+                    } else {
+                        cout << "Sorry, this book is unavailable." << endl;
+                    }
+                    break;
+                }
+            }
+
+            if (!found) {
+                cout << "ISBN " << isbn << " not found!" << endl;
+            }
+
+            file.close();
+        }
+
+        void returnBook(){
+            system("cls");
+            string isbn;
+            cout << "Enter the ISBN to return: ";
+            cin >> isbn;
+
+            fstream file("books.dat", ios::in|ios::out|ios::binary);
+            
+            bool found = false;
+            while (file.read(reinterpret_cast<char*>(this), sizeof(*this))) {
+                if (ISBN == isbn) {
+                    found = true;
+                    file.seekp(-static_cast<int>(sizeof(*this)), ios::cur);
+                    file.write(reinterpret_cast<char*>(this), sizeof(*this));
+                    cout << bookTitle << " returned." << endl;
+                    break;
+                }
+            }
+
+            if (!found) {
+                cout << "Book with ISBN " << isbn << " not found." << endl;
+            }
+
+            file.close();
+        }
+
+        void updateBook(){
+            system("cls");
+            string isbn;
+            cout << "Enter the ISBN to update: ";
+            cin >> isbn;
+            cin.ignore();
+
+            fstream file("books.dat", ios::binary | ios::in | ios::out);
+            if (!file) {
+                cerr << "Error opening file." << endl;
+                return;
+            }
+
+            bool found = false;
+            while (file.read(reinterpret_cast<char *>(this), sizeof(*this))) {
+                if (ISBN == isbn) {
+                    found = true;
+                    cout << "Update details:" << endl;
+                    getData();
+                    file.seekp(-static_cast<int>(sizeof(*this)), ios::cur);
+                    file.write(reinterpret_cast<char *>(this), sizeof(*this));
+                    cout << "Book updated." << endl;
+                    break;
+                }
+            }
+
+            if (!found) {
+                cout << "Book with " << isbn << " not found." << endl;
+            }
+
+            file.close();
+        }
+
+        void deleteBook(){
+            system("cls");
+            string isbn;
+            cout << "Enter the ISBN to delete: ";
+            cin >> isbn;
+            cin.ignore();
+
+            ifstream inf("books.dat", ios::binary);
+            if (!inf) {
+                cerr << "Error to open file." << endl;
+                return;
+            }
+
+            ofstream outf("temp.dat", ios::binary);
+            if (!outf) {
+                cerr << "Error creating temporary file." << endl;
+                inf.close();
+                return;
+            }
+
+            bool found = false;
+            while (inf.read(reinterpret_cast<char *>(this), sizeof(*this))) {
+                if (ISBN != isbn) {
+                    outf.write(reinterpret_cast<const char *>(this), sizeof(*this));
+                } else {
+                    found = true;
+                }
+            }
+
+            inf.close();
+            outf.close();
 
             if (found) {
-                tempFile << book.ISBN << ";" << line.substr(pos + 1, line.find_last_of(';') - pos - 1) << ";" << book.numberOfCopies << endl;
-            }
-        }
-
-        if (!found) {
-            cerr << "Book with ISBN " << isbn << " not found.\n";
-        }
-
-        file.close();
-        tempFile.close();
-        remove(catName.c_str());
-        rename("temp.txt", catName.c_str());
-    }
-
-    void returnBook() {
-        cout << "Enter ISBN to return: ";
-        string isbn;
-        cin>> isbn;
-
-        ifstream file(catName);
-        ofstream tempFile("temp.txt");
-
-        string line;
-        bool found = false;
-
-        while (getline(file, line)) {
-            Book book;
-            size_t pos = line.find(';');
-            book.ISBN = line.substr(0, pos);
-
-            if (book.ISBN == isbn) {
-                found = true;
-                book.numberOfCopies = stoi(line.substr(line.find_last_of(';') + 1)) + 1;
+                remove("books.dat");
+                rename("temp.dat", "books.dat");
+                cout << "Book deleted." << endl;
             } else {
-                tempFile << line << endl;
-            }
-
-            if (found) {
-                tempFile << book.ISBN << ";" << line.substr(pos + 1, line.find_last_of(';') - pos - 1) << ";" << book.numberOfCopies << endl;
+                cout << "ISBN " << isbn << " not found." << endl;
+                remove("temp.dat");
             }
         }
 
-        if (!found) {
-            cerr << "Book with ISBN " << isbn << " not found.\n";
-        }
-
-        file.close();
-        tempFile.close();
-        remove(catName.c_str());
-        rename("temp.txt", catName.c_str());
-    }
-
-    void updateBookDetails() {
-        Book updatedBook;
-        updatedBook.inputBook();
-
-        ifstream file(catName);
-        ofstream tempFile("temp.txt");
-
-        string line;
-        bool found = false;
-
-        while (getline(file, line)) {
-            Book book;
-            size_t pos = line.find(';');
-            book.ISBN = line.substr(0, pos);
-
-            if (book.ISBN == updatedBook.ISBN) {
-                found = true;
-                tempFile << updatedBook.ISBN << ";" << updatedBook.bookTitle << ";" << updatedBook.author << ";" << updatedBook.numberOfCopies << endl;
-            } else {
-                tempFile << line << endl;
+        void listBooks() const {
+            system("cls");
+            cout << "BOOKS" << endl << endl;
+            ifstream inf("books.dat", ios::binary | ios::in);
+            if (!inf) {
+                cerr << "Error opening file!" << endl;
+                return;
             }
-        }
 
-        if (!found) {
-            cerr << "Book with ISBN " << updatedBook.ISBN << " not found.\n";
-        }
-
-        file.close();
-        tempFile.close();
-        remove(catName.c_str());
-        rename("temp.txt", catName.c_str());
-    }
-
-    void deleteBook() {
-        cout << "Enter ISBN to delete: ";
-        string isbn;
-        cin>> isbn;
-
-        ifstream file(catName);
-        ofstream tempFile("temp.txt");
-
-        string line;
-        bool found = false;
-
-        while (getline(file, line)) {
-            Book book;
-            size_t pos = line.find(';');
-            book.ISBN = line.substr(0, pos);
-
-            if (book.ISBN == isbn) {
-                found = true;
-                int copies = stoi(line.substr(line.find_last_of(';') + 1));
-                if (copies > 0) {
-                    cerr << "Error: Cannot delete a book with available copies.\n";
-                    tempFile << line << endl;
-                }
-            } else {
-                tempFile << line << endl;
+            Library temp;
+            while (inf.read(reinterpret_cast<char*>(&temp), sizeof(temp))) {
+                temp.showData();
+                cout << endl;
             }
+
+            inf.close();
+            cout << "End" << endl;
         }
 
-        if (!found) {
-            cerr << "Error: Book with ISBN " << isbn << " not found.\n";
-        }
-
-        file.close();
-        tempFile.close();
-        remove(catName.c_str());
-        rename("temp.txt", catName.c_str());
-    }
 };
 
-int main() {
-    Library lib("catalogue.txt");
-    lib.addNewBook();
-    lib.issueBook();
-    lib.returnBook();
-    lib.updateBookDetails();
-    lib.deleteBook();
-    return 0;
+int main(){
+    char ch;
+    Library lib;
+
+    while(true){
+        system("cls");
+        cout<<"\t\t\t\t\t\t\tBOOKS\n";
+        cout<<"\t\t\t\t\t\t1.Add Book\n";
+        cout<<"\t\t\t\t\t\t2.Issue Book\n";
+        cout<<"\t\t\t\t\t\t3.Return Book\n";
+        cout<<"\t\t\t\t\t\t4.Update Book\n";
+        cout<<"\t\t\t\t\t\t5.Delete Book\n";
+        cout<<"\t\t\t\t\t\t6.Booklist\n";
+        cout<<"\t\t\t\t\t\t7.Exit\n";
+        cout<<"Enter your choice: ";
+        cin >> ch;
+
+        switch(ch){
+            case '1':
+                lib.addBook();
+                break;
+            
+            case '2':
+                lib.issueBook();
+                break;
+            
+            case '3':
+                lib.returnBook();
+                break;
+            
+            case '4':
+                lib.updateBook();
+                break;
+            
+            case '5':
+                lib.deleteBook();
+                break;
+            
+            case '6':
+                lib.listBooks();
+                break;
+
+            case '7':
+                exit(0);
+            
+            default:
+                cout << "Invalid.";
+        }
+
+        cout << endl << "Press any key to continue:";
+        cin.ignore();
+        getch();
+    }
 }
